6057616eff45a6123b5604325a585feedb057786
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-25 01:09:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show 
     Show.order(rating: :desc).limit(1).first
-    # 
+    # self.where(rating = ?)
   end 
   
   def self.lowest_rating 
