34e5e6368428b6781337a08ed614b90da6a614d1
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-25 01:08:21 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,31 +1,31 @@
 class Show < ActiveRecord::Base 
   
-  def Show::highest_rating 
+  def self.highest_rating 
     Show.maximum(:rating)
   end 
   
-  def Show::most_popular_show 
+  def self.most_popular_show 
     Show.order(rating: :desc).limit(1).first
   end 
   
-  def Show::lowest_rating 
+  def self.lowest_rating 
     Show.minimum(:rating)
     # returns only the rating, not the Show object
   end 
   
-  def Show::least_popular_show 
+  def self.least_popular_show 
     Show.order(rating: :asc).limit(1).first
   end  
   
-  def Show::ratings_sum 
+  def self.ratings_sum 
     Show.sum(:rating)
   end 
   
-  def Show::popular_shows 
+  def self.popular_shows 
     Show.where("rating > ?", 5)
   end 
   
-  def Show::shows_by_alphabetical_order 
+  def self.shows_by_alphabetical_order 
     Show.order(name: :asc)
   end 
   
